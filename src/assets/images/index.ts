055e432c249488386afcd6a76b9dac723fc81fcc
zksdex <128878508+zksdex@users.noreply.github.com>
2023-04-01 10:50:47 +0800

    Add files via upload

---
+++ src/assets/images/index.ts
@@ -0,0 +1,90 @@
+import ETH from './eth.png'
+import BNB from './bnb.png'
+import MATIC from './matic.png'
+import ARBITRUM from './arbitrum.png'
+import OPTIMISTIC_ETH from './optimisticEth.svg'
+import XDAI from './xdai.png'
+import RBTC from './rbtc.png'
+import AVAX from './avax.png'
+import TOMO from './tomo.svg'
+import CRO from './cro.svg'
+import HT from './ht.svg'
+import GLMR from './glmr.png'
+import AURORA from './aurora.svg'
+import FTM from './ftm.svg'
+import FUSE from './fuse.svg'
+import MOVR from './movr.svg'
+import ONE from './one.svg'
+import AME from './ame.png'
+import BTCIX from './btcix.png'
+import CNDL from './cndl.svg'
+import EDG from './edg.svg'
+
+export const CURRENCY = {
+  ETH,
+  BNB,
+  MATIC,
+  ARBITRUM,
+  OPTIMISTIC_ETH,
+  AVAX,
+  XDAI,
+  RBTC,
+  TOMO,
+  CRO,
+  HT,
+  GLMR,
+  AURORA,
+  FTM,
+  FUSE,
+  MOVR,
+  ONE,
+  AME,
+  BTCIX,
+  CNDL,
+  EDG,
+
+  // ------------
+
+  1: ETH,
+  3: ETH,
+  4: ETH,
+  5: ETH,
+  42: ETH,
+  56: BNB,
+  97: BNB,
+  137: MATIC,
+  80001: MATIC,
+  42161: ARBITRUM,
+  421611: ARBITRUM,
+  10: OPTIMISTIC_ETH,
+  69: OPTIMISTIC_ETH,
+  100: XDAI,
+  77: XDAI,
+  30: RBTC,
+  31: RBTC,
+  43114: AVAX,
+  43113: AVAX,
+  88: TOMO,
+  89: TOMO,
+  25: CRO,
+  338: CRO,
+  128: HT,
+  256: HT,
+  1284: GLMR,
+  1287: GLMR,
+  1313161554: AURORA,
+  1313161555: AURORA,
+  250: FTM,
+  4002: FTM,
+  122: FUSE,
+  123: FUSE,
+  1285: MOVR,
+  1666600000: ONE,
+  180: AME,
+  19845: BTCIX,
+  534: CNDL,
+  2021: EDG,
+  
+  280: ETH,
+  324: ETH,
+}
